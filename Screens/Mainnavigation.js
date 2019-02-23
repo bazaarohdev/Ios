2dfc33f5e96bd9b4b06572b5d7fdc2e380cf62df
bazaarohdev <46267415+bazaarohdev@users.noreply.github.com>
2019-02-23 22:32:28 +0530

    Update Mainnavigation.js

--- Screens/Mainnavigation.js
+++ Screens/Mainnavigation.js
@@ -6,8 +6,6 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
 
 import { StackNavigator } from 'react-navigation';
 import Login from './Loginscreen'
